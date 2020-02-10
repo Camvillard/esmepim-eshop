39fe49954906aed5600c0d0cf3f63c6c73fcabd0
camille villard <hello@camillevillard.com>
2020-02-10 18:45:10 -0500

    init repo - react app w/ typescript & styled components

---
+++ src/components/Landing/Landing.component.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import { Input } from '../Input/Input.component'
+
+
+export const Landing = () => {
+  return(
+    <Fragment>
+      <Input />
+    </Fragment>
+  )
+}
